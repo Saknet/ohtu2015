e92a5b2f750e72bebffc9bd146a108904ded97cb
Saknet <andreas.andra@helsinki.fi>
2015-03-20 17:48:31 +0200

    tehtavat10 ja 11

--- viikko2/Verkkokauppa1teh11/src/main/java/ohtu/Main.java
+++ viikko2/Verkkokauppa1teh11/src/main/java/ohtu/Main.java
@@ -10,7 +10,7 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {       
         ApplicationContext ctx = new FileSystemXmlApplicationContext("src/main/resources/spring-context.xml");
         Kauppa kauppa = ctx.getBean(Kauppa.class);
 
---
+++ viikko2/Verkkokauppa1teh11/src/main/java/ohtu/verkkokauppa/ViiteInterface.java
@@ -0,0 +1,8 @@
+
+package ohtu.verkkokauppa;
+
+public interface ViiteInterface {
+
+    int uusi();
+    
+}
