bba069aa7ea3ab3185dfba5c668d6d5b4bfd8bb8
Saknet <andreas.andra@helsinki.fi>
2015-04-08 23:53:11 +0300

    viikko4 kaikki

--- viikko4/LoginWeb2/src/main/java/ohtu/Tester.java
+++ viikko4/LoginWeb2/src/main/java/ohtu/Tester.java
@@ -27,5 +27,102 @@ public class Tester {
         System.out.println("==");
         System.out.println( driver.getPageSource() );
         
+        unsuccesfulLoginWrongPassword(driver);
+        unsuccesfulLoginNoUsername(driver);
+        creatingNewUser(driver);
+        createUserAndLogout(driver);
+    }
+    
+    public static void unsuccesfulLoginWrongPassword(WebDriver driver) {
+        driver.get("http://localhost:8090");
+        System.out.println( driver.getPageSource() );
+        WebElement element = driver.findElement(By.linkText("login"));       
+        element.click(); 
+        
+        System.out.println("==");
+        
+        System.out.println( driver.getPageSource() );
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekka");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("aep");
+        element = driver.findElement(By.name("login"));
+        element.submit();
+        
+        System.out.println("==");
+        System.out.println( driver.getPageSource() );        
+    }
+    
+    public static void unsuccesfulLoginNoUsername(WebDriver driver) {
+        driver.get("http://localhost:8090");
+        System.out.println( driver.getPageSource() );
+        WebElement element = driver.findElement(By.linkText("login"));       
+        element.click(); 
+        
+        System.out.println("==");
+        
+        System.out.println( driver.getPageSource() );
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekkasdfsdfs");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("aep");
+        element = driver.findElement(By.name("login"));
+        element.submit();
+        
+        System.out.println("==");
+        System.out.println( driver.getPageSource() );         
+    }
+    
+    public static void creatingNewUser(WebDriver driver) {
+        driver.get("http://localhost:8090");
+        System.out.println( driver.getPageSource() );
+        WebElement element = driver.findElement(By.linkText("register new user"));       
+        element.click(); 
+        
+        System.out.println("==");
+        
+        System.out.println( driver.getPageSource() );
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("jeesus");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("testpass1");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("testpass1");
+        element = driver.findElement(By.name("add"));
+        element.submit();
+        
+        System.out.println("==");
+        System.out.println( driver.getPageSource() );    
+    }
+    
+    public static void createUserAndLogout(WebDriver driver) {
+        driver.get("http://localhost:8090");
+        System.out.println( driver.getPageSource() );
+        WebElement element = driver.findElement(By.linkText("register new user"));       
+        element.click(); 
+        
+        System.out.println("==");
+        
+        System.out.println( driver.getPageSource() );
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("peasdfsdfs");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("testpass1");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("testpass1");
+        element = driver.findElement(By.name("add"));
+        element.submit();
+        
+        System.out.println("==");
+        System.out.println( driver.getPageSource() ); 
+        
+        element = driver.findElement(By.linkText("continue to application mainpage"));  
+        element.click();  
+        System.out.println("==");
+        element = driver.findElement(By.linkText("logout"));       
+        element.click(); 
+        
+        System.out.println("==");
+        System.out.println( driver.getPageSource() ); 
     }
 }
